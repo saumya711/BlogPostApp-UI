c5d758e4718496e8a16556b378c9c7c5a5f7077a
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-06-11 23:23:04 +0530

    CHG | Create ANgular Services and Complete Create Category

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from './../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
 
-  constructor() {
+  constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -18,6 +21,17 @@ export class AddCategoryComponent {
   }
 
   onFormSubmit() {
-    console.log(this.model);
+    //console.log(this.model);
+    this.addCategorySubscription = this.categoryService.addCategory(this.model)
+    .subscribe({
+      next: (response) => {
+        console.log("This was successful!");
+      }
+    })
   }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
+  }
+
 }
