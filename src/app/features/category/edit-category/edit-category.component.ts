245306992538b9b8b0a5a9cee90eec3034ed015e
Niroshani Saumya <36502790+saumya711@users.noreply.github.com>
2024-06-17 21:36:40 +0530

    CHG | Create Service method and display single category

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Category } from '../models/category.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,19 +13,35 @@ export class EditCategoryComponent implements OnInit, OnDestroy{
 
   id: string | null = null;
   paramsSubscription?: Subscription;
+  category?: Category;
 
-  constructor (private route: ActivatedRoute) {}
+  constructor (private route: ActivatedRoute, private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if(this.id) {
+          // get the data from API for this category Id
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next: (response) => {
+              this.category = response;
+            }
+          });
+        }
+
       }
     });
+  }
+
+  onFormSubmit() : void {
+    console.log(this.category)
   }
 
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
   }
-  
+
 }
